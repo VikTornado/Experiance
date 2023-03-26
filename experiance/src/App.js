6e929b03ad4074eb4615eb589d4433ffa3391359
React <3809164@gmail.com>
2023-03-26 17:54:10 +0300

    creat HeroPage

--- experiance/src/App.js
+++ experiance/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
+import Hero from "./Components/Hero";
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Hero/>
     </div>
   );
 }
---
+++ experiance/src/Components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div>
+            <img src={"https://wallpapershome.com/images/pages/pic_h/19568.jpg"} alt={"WPhoto"}/>
+        </div>
+    );
+};
+
+export default Hero;
